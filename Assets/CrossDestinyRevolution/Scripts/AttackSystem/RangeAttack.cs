c6c467cbcd9538140f25748e47d584f0745a7d8a
ULTIMATUM9000 <kylepogideleon@gmail.com>
2022-05-17 14:04:47 +0800

    Attack System and some cleanup

---
+++ Assets/CrossDestinyRevolution/Scripts/AttackSystem/RangeAttack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CDR.ActionSystem;
+
+namespace CDR.AttackSystem
+{
+	public class RangeAttack : Action
+	{
+
+	}
+}
+
