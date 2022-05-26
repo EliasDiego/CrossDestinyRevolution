a1ace54b46e4b8a6ce03169f8ed0b36370e48293
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-05-26 14:10:20 +0800

    Duplicated Sample Scene for Test Scene (Used sample scene for testing XD), Removed Commented Old Sequence Code

--- Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/LoadSceneHandler.cs
+++ Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/LoadSceneHandler.cs
@@ -40,45 +40,6 @@ namespace CDR.SceneManagementSystem
             yield return UnloadScene(SceneManager.GetSceneByBuildIndex(_SceneLoader.loadingScreenSceneIndex));
         }
         
-        // private IEnumerator Sequence()
-        // {
-        //     _Camera.gameObject.SetActive(false);
-
-        //     // Unload Previous Scene
-        //     yield return UnloadScene(SceneManager.GetActiveScene());
-
-        //     _Camera.gameObject.SetActive(true);
-
-        //     yield return new WaitForSeconds(1);
-
-        //     // Create Temp Scene
-        //     Scene tempScene = SceneManager.CreateScene("Temp Scene");
-
-        //     SceneManager.SetActiveScene(tempScene);
-
-        //     yield return new WaitForSeconds(1);
-
-        //     // Load Next Scene
-
-        //     yield return LoadScene(contextData.sceneIndex, LoadSceneMode.Additive, OnBeforeSceneActivation);
-
-        //     yield return new WaitForSeconds(1);
-
-        //     Scene nextScene = SceneManager.GetSceneByBuildIndex(contextData.sceneIndex);
-
-        //     SceneManager.SetActiveScene(nextScene);
-
-        //     foreach(GameObject gameObject in tempScene.GetRootGameObjects())
-        //         SceneManager.MoveGameObjectToScene(gameObject, nextScene);
-
-        //     yield return UnloadScene(tempScene);
-
-        //     yield return new WaitForSeconds(1);
-
-        //     // // Unload Loading Screen
-        //     yield return UnloadScene(SceneManager.GetSceneByBuildIndex(_SceneLoader.loadingScreenSceneIndex));
-        // }
-
         public IEnumerator UnloadScene(Scene scene)
         {
             AsyncOperation asyncOperation = SceneManager.UnloadSceneAsync(scene);
