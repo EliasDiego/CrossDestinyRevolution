36f2784df72adba0d4684a9790c503c4df784fc8
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-05-26 16:54:33 +0800

    Fixed Conflicts

--- Assets/CrossDestinyRevolution/Scripts/AttackSystem/RangeAttack.cs
+++ Assets/CrossDestinyRevolution/Scripts/AttackSystem/RangeAttack.cs
@@ -5,11 +5,7 @@ using CDR.ActionSystem;
 
 namespace CDR.AttackSystem
 {
-<<<<<<< HEAD
-	public class RangeAttack : CooldownAction
-=======
 	public class RangeAttack : CooldownAction , IRangeAttack
->>>>>>> d119aa0fce11f2afa10cef419cd99ea93b1811fa
 	{
 		[SerializeField] float FireRate;
 
@@ -18,13 +14,10 @@ namespace CDR.AttackSystem
 
 		[SerializeField] GameObject BulletProjectile;
 
-<<<<<<< HEAD
-=======
 		public IProjectile projectile => throw new System.NotImplementedException();
 
 		public float range => throw new System.NotImplementedException();
 
->>>>>>> d119aa0fce11f2afa10cef419cd99ea93b1811fa
 		private void Start()
 		{
 			_cooldownDuration = FireRate;
@@ -33,20 +26,9 @@ namespace CDR.AttackSystem
 		public override void Update()
 		{
 			base.Update();
-<<<<<<< HEAD
-
-			BulletProjectile.GetComponent<Projectile>().projectileTarget = TargetPoint.transform.position;
-			BulletProjectile.GetComponent<Projectile>().projectileOriginPoint = GunPoint.transform.position;
-
-			if (Input.GetMouseButton(0) && !_isCoolingDown) //test
-			{
-				Use();
-			}
-=======
 			
 			BulletProjectile.GetComponent<Projectile>().projectileTarget = SetPredictiveTarget();
 			BulletProjectile.GetComponent<Projectile>().projectileOriginPoint = GunPoint.transform.position;
->>>>>>> d119aa0fce11f2afa10cef419cd99ea93b1811fa
 		}
 
 		public override void Use()
@@ -65,10 +47,6 @@ namespace CDR.AttackSystem
 
 		Vector3 SetPredictiveTarget()
 		{
-<<<<<<< HEAD
-			var direction = (TargetPoint.position - GunPoint.transform.position).normalized;
-			return direction;
-=======
 			var currentTarget = Character.targetHandler.GetCurrentTarget();
 			var targetVelocity = currentTarget.activeCharacter.controller.velocity;
 			
@@ -84,7 +62,6 @@ namespace CDR.AttackSystem
 			{
 				return currentTarget.activeCharacter.position;
 			}
->>>>>>> d119aa0fce11f2afa10cef419cd99ea93b1811fa
 		}
 
 		void SetProjectile()
