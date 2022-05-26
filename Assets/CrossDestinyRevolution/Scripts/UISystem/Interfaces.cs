717553369e70952ddcdb4d9ee74b372fe3f0bf85
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-05-26 14:19:08 +0800

    Added interfaces for Battle UI(Not yet sure on some details but generally okay) and Menu. Created Test Script for Battle UI

---
+++ Assets/CrossDestinyRevolution/Scripts/UISystem/Interfaces.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CDR.MechSystem;
+using CDR.AttackSystem;
+using CDR.ActionSystem;
+using CDR.MovementSystem;
+
+namespace CDR.UISystem
+{
+    public interface IUIElement
+    {
+        bool isShown { get; }
+
+        void Show();
+        void Hide();
+    }
+
+    public interface ICooldownActionUI : IUIElement
+    {
+        void SetCooldownAction(ICooldownAction cooldownAction);
+    }
+
+    public interface IValueRangeUI : IUIElement
+    {
+        void SetValueRange(IValueRange valueRange);
+    }
+
+    public interface IPlayerMechBattleUI : IUIElement
+    {
+        IValueRangeUI healthUI { get; }
+        IValueRangeUI boostUI { get; }
+        ICooldownActionUI rangeAttackUI { get; }
+        ICooldownActionUI specialAttack1AttackUI { get; }
+        ICooldownActionUI specialAttack2AttackUI { get; }
+        ICooldownActionUI specialAttack3AttackUI { get; }
+        
+        void SetMech(IMech mech);
+    }
+
+    public interface IMenu : IUIElement
+    {
+        IMenu previousMenu { get; }
+        void SwitchTo(IMenu nextMenu);
+        void Back();
+    }
+}
