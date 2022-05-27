b314500e1f57f5f5402028dd480120aeed445bad
andrei-excel <andrei.excel2@gmail.com>
2022-05-27 20:16:30 +0800

    Updated Health script

--- Assets/CrossDestinyRevolution/Scripts/MechSystem/Health.cs
+++ Assets/CrossDestinyRevolution/Scripts/MechSystem/Health.cs
@@ -5,26 +5,8 @@ using UnityEngine;
 
 namespace CDR.MechSystem
 {
-    public class Health : MonoBehaviour, IHealth
+    public class Health : ValueRange, IHealth
     {
-        [SerializeField] ValueRange _valueRange;
-
-
-        public float CurrentValue => _valueRange.CurrentValue;
-        public float MaxValue => _valueRange.MaxValue;
-        public event Action OnModifyValue;
-
-        public void ModifyValue(float value)
-        {
-            ModifyValueWithoutEvent(value);
-            OnModifyValue?.Invoke();
-        }
-
-        public void ModifyValueWithoutEvent(float value)
-        {
-            _valueRange.ModifyValueWithoutEvent(value);
-        }
-
         public void CheckHealthStatus()
         {
             if(CurrentValue < 0)
@@ -37,16 +19,12 @@ namespace CDR.MechSystem
         {
             float currentHealth = CurrentValue - damage;
             ModifyValue(currentHealth);
+            CheckHealthStatus();
         }
 
         public void Death()
         {
             Debug.Log("Player died");
-        }
-
-        private void Update()
-        {
-            CheckHealthStatus();
         }
     }
 }
