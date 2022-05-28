cc61d841263ce491fe31ab64744ed9b48d53c357
A-Cabigao <91714998+A-Cabigao@users.noreply.github.com>
2022-05-28 16:01:42 +0800

    Rewrote boost code to use rigidbody. Boost now follows current direction. Fixed character clipping on vertical down boost. Clamped z for horizontal movement.

--- Assets/CrossDestinyRevolution/Scripts/MovementSystem/Controller/Controller.cs
+++ Assets/CrossDestinyRevolution/Scripts/MovementSystem/Controller/Controller.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 namespace CDR.MovementSystem
 {
-    public class Controller : MonoBehaviour , IController
+    public class Controller : MonoBehaviour , ICharacterController
     {
         [SerializeField]
         private FlightPlane _flightPlane;
 
         private Rigidbody rb;
 
-        public IFlightPlane flightPlane => _flightPlane;
-
         public Vector3 velocity => rb.velocity;
+
+        public IFlightPlane flightPlane 
+        { 
+            get => _flightPlane; 
+            set => _flightPlane = (FlightPlane)value; 
+        }
 
         private void Awake()
         {
@@ -30,20 +34,19 @@ namespace CDR.MovementSystem
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, magnitude);
         }
 
-        public void MoveRb(Vector3 force)
+        public void AddRbForce(Vector3 force, ForceMode mode = ForceMode.VelocityChange)
         {
-            rb.AddForce(force, ForceMode.VelocityChange);
+            rb.AddForce(force, mode);
         }
 
         public void Translate(Vector3 direction, float magnitude)
         {
-            transform.Translate(direction * magnitude);
+            transform.position = direction * magnitude;
         }
 
         public void Rotate(Quaternion rotation)
         {
-            transform.rotation = rotation;
-            
+            transform.rotation = rotation;           
         }
     }
 }
