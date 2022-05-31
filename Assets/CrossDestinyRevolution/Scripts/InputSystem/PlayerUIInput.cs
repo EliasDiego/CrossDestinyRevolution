ae3477fdbf1d937b1e4e0a2af6ae50c642f67a26
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-05-31 15:14:30 +0800

    Created Player UI Input (Don't like working with Unity Scripts XD Might regret it though)

--- Assets/CrossDestinyRevolution/Scripts/InputSystem/PlayerUIInput.cs
+++ Assets/CrossDestinyRevolution/Scripts/InputSystem/PlayerUIInput.cs
@@ -4,13 +4,118 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Users;
+using UnityEngine.EventSystems;
 
 namespace CDR.InputSystem
 {
     public class PlayerUIInput : PlayerInput
     {
-        
+        private Selectable _CurrentSelectable;
+        private Vector2 _MousePosition;
+
+        private bool _IsClicked = false;
+
+        private RectTransform _RectTransform;
+
+        public Selectable currentSelectable { get => _CurrentSelectable; set => SetCurrentSelectable(value); }
+
+        private void Awake() 
+        {
+            _RectTransform = (RectTransform)transform;
+        }
+
+        private void SetCurrentSelectable(Selectable selectable)
+        {
+            _CurrentSelectable = selectable;
+
+            if(!selectable)
+                return;
+                
+            RectTransform selectableRectTransform = selectable.transform as RectTransform;
+
+            _RectTransform.position = selectableRectTransform.position; 
+            _RectTransform.sizeDelta = selectableRectTransform.sizeDelta;
+        }
+
+        private bool IsInsideRect(RectTransform rectTransform, Vector2 point)
+        {
+            Rect rect = new Rect(rectTransform.position - (Vector3)rectTransform.sizeDelta / 2, rectTransform.sizeDelta);
+            
+            return rect.Contains(point);
+        }
+
+        private void OnNavigate(InputAction.CallbackContext context)
+        {
+            if(!currentSelectable)
+            {
+                currentSelectable = Selectable.allSelectablesArray.FirstOrDefault();
+
+                return;
+            }
+
+            Selectable nextSelectable = currentSelectable.FindSelectable(context.ReadValue<Vector2>());
+
+            if(nextSelectable)
+                currentSelectable = nextSelectable;
+        }
+
+        private void OnSubmit(InputAction.CallbackContext context)
+        {
+            if(!currentSelectable)
+                return;
+
+            if(currentSelectable is ISubmitHandler s)
+                s.OnSubmit(null);
+        }
+
+        private void OnCancel(InputAction.CallbackContext context)
+        {
+            if(!currentSelectable)
+                return;
+
+            if(currentSelectable is ICancelHandler c)
+                c.OnCancel(null);
+        }
+
+        private void OnPoint(InputAction.CallbackContext context)
+        {
+            _MousePosition = context.ReadValue<Vector2>();
+
+            Selectable selectable = Selectable.allSelectablesArray.FirstOrDefault(s => IsInsideRect(s.transform as RectTransform, _MousePosition));
+
+            if(currentSelectable && currentSelectable != selectable)
+                currentSelectable.OnPointerExit(null); 
+
+            if(selectable)
+            {
+                currentSelectable = selectable;
+                currentSelectable.OnPointerEnter(null);
+            }
+        }
+
+        private void OnClick(InputAction.CallbackContext context)
+        {
+            if(!currentSelectable)
+                return;
+
+            _IsClicked = !_IsClicked;
+
+            if(!_IsClicked && currentSelectable is ISubmitHandler s)
+                s.OnSubmit(null);
+        }
+
+        public override void SetupInput(InputActionMap inputActionMap, params InputDevice[] devices)
+        {
+            base.SetupInput(inputActionMap, devices);
+
+            inputActions["Point"].performed += OnPoint;
+            inputActions["Navigate"].started += OnNavigate;
+            inputActions["Navigate"].performed += OnNavigate;
+            inputActions["Submit"].started += OnSubmit;
+            inputActions["Cancel"].started += OnCancel;
+            inputActions["Click"].performed += OnClick;
+        }
     }
 }
