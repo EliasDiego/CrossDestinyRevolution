6b95e2faedc66728e654f393f066235cfc320b4b
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-06-16 19:33:25 +0800

    Imported New Animation Mech. Created Test Script for float or bool

--- Assets/CrossDestinyRevolution/Scripts/AnimationSystem/Test/TestAnimation.cs
+++ Assets/CrossDestinyRevolution/Scripts/AnimationSystem/Test/TestAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 namespace CDR.AnimationSystem
 {
@@ -13,18 +14,52 @@ namespace CDR.AnimationSystem
         [SerializeField, Range(-1, 1)]
         float _MoveY = 0;
 
+        [SerializeField]
+        bool _UseMeleeAttackTime = false;
+        [SerializeField]
+        bool _IsMeleeAttack = false;
+        [SerializeField]
+        float _MeleeAttackTime = 0;
+
         Animator _Animator;
 
         private void Awake() 
         {
             _Animator = GetComponent<Animator>();
+
+                StartCoroutine(MeleeAttack());
         }
 
-        private void Update() 
+        // private void Update() 
+        // {
+        //     _Animator.SetBool("IsMove", _IsMove);
+        //     _Animator.SetFloat("MoveX", _MoveX);
+        //     _Animator.SetFloat("MoveY", _MoveY);    
+        // }
+
+        private IEnumerator MeleeAttack()
         {
-            _Animator.SetBool("IsMove", _IsMove);
-            _Animator.SetFloat("MoveX", _MoveX);
-            _Animator.SetFloat("MoveY", _MoveY);    
+            float currentTime = 0;
+            
+            if(_UseMeleeAttackTime)
+                _Animator.SetFloat("MeleeAttackTime", 1 / _MeleeAttackTime);
+            else
+                _Animator.SetBool("IsMeleeAttack", true);
+
+            while(currentTime < _MeleeAttackTime)
+            {
+                currentTime += Time.deltaTime;
+
+                // _Animator.SetFloat("MeleeAttackTime", currentTime / _MeleeAttackTime);
+
+                yield return null;
+            
+            }
+
+            if(_UseMeleeAttackTime)
+                _Animator.SetFloat("MeleeAttackTime", 0);
+            else
+                _Animator.SetBool("IsMeleeAttack", false);
         }
     }
 }
