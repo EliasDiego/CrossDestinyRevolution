15079d021c965560f5fd094c4d36bad0c5c4b3ca
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-06-20 03:02:09 +0800

    Removed Addressables, Removed Audio Listener in Camera in Loading Screen

--- Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/LoadSceneHandler.cs
+++ Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/LoadSceneHandler.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.AddressableAssets;
 
 namespace CDR.SceneManagementSystem
 {
--- Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/Test.cs
+++ Assets/CrossDestinyRevolution/Scripts/SceneManagementSystem/Test.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.AddressableAssets;
 
 using CDR.SceneManagementSystem;
 
