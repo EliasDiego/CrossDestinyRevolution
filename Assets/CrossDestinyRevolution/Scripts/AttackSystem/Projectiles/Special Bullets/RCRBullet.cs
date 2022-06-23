85cc791c3964bac1fd07022f48f6ec8c8591de4d
A-Cabigao <91714998+A-Cabigao@users.noreply.github.com>
2022-06-23 11:49:01 +0800

    Added rotating and scaling for special attack.

--- Assets/CrossDestinyRevolution/Scripts/AttackSystem/Projectiles/Special Bullets/RCRBullet.cs
+++ Assets/CrossDestinyRevolution/Scripts/AttackSystem/Projectiles/Special Bullets/RCRBullet.cs
@@ -8,16 +8,34 @@ namespace CDR.AttackSystem
     {
         [Header("Unique properties")]
         [SerializeField]
-        private Transform pivot;
+        private GameObject pivot;
+        [SerializeField]
+        private GameObject beam;
         [SerializeField]
         private float maxLength;
         [SerializeField]
         private float timeToScale;
+        [SerializeField]
+        private float rotationSpeed;
+
+        private bool enableRotation = false;
+
+        private void Awake()
+        {
+            HitBox.onHitEnter += OnHitEnter;        
+        }
 
         public override void OnEnable()
         {
             base.OnEnable();
-            HitBox.onHitEnter += OnHitEnter;
+            ScaleWithTime();
+            //Debugger();
+        }
+
+        private void Debugger()
+        {
+            enableRotation = true;
+            beam.transform.localScale = new Vector3(1f, 1f, maxLength);
         }
 
         private void OnDestroy()
@@ -25,14 +43,50 @@ namespace CDR.AttackSystem
             HitBox.onHitEnter -= OnHitEnter;            
         }
 
-        private void FixedUpdate()
+        public override void Update()
         {
-            AddRbForce(transform.forward);
+            base.Update();
+            Rotate();
         }
+
+        //private void FixedUpdate()
+        //{
+        //    AddRbForce(new Vector3(0f, 0f, 0.01f));
+        //}
 
         protected override void OnHitEnter(IHitEnterData hitData)
         {
             Debug.Log("ALSKNd");
         }
+        public override void ResetObject()
+        {
+            enableRotation = false;
+            pivot.transform.localScale = Vector3.zero;
+            transform.localEulerAngles.Set(0f, 90f, 0f);
+        }
+
+        private void ScaleWithTime()
+        {
+            LeanTween.scale(beam, Vector3.one, 0.45f).setOnComplete(()=>
+            {
+                LeanTween.scaleZ(pivot, maxLength, timeToScale).setOnComplete(
+                () =>
+                {
+                    enableRotation = true;
+                });
+            });
+            
+        }
+
+        private void Rotate()
+        {
+            if (Mathf.Floor(270f - transform.localEulerAngles.y) != 0 && enableRotation)
+            {
+                transform.RotateAround(transform.position, transform.up, -rotationSpeed * Time.deltaTime);
+            }
+
+            //Rotate(Quaternion.Euler(0f, transform.localEulerAngles.y - (rotationSpeed * Time.deltaTime), 0f));
+        }
+
     }
 }
