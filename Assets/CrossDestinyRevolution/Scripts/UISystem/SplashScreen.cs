115f93c090f987fd7183450ae54180afe2c44bca
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-07-07 23:59:00 +0800

    Updated splash screen

--- Assets/CrossDestinyRevolution/Scripts/UISystem/SplashScreen.cs
+++ Assets/CrossDestinyRevolution/Scripts/UISystem/SplashScreen.cs
@@ -2,26 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+using CDR.VFXSystem;
+using CDR.InputSystem;
 
 namespace CDR.UISystem
 {
-    public class SplashScreen : MonoBehaviour, IUIElement
+    public class SplashScreen : Menu, IMenuSubmitHandler
     {
-        public bool isShown => false;
+        [Header("Fade")]
+        [SerializeField]
+        private Gradient _FadeGradient;
+        [SerializeField]
+        private Image _Panel;
+        [SerializeField]
+        private float _FadeInTime;
+        [SerializeField]
+        private float _FadeOutTime;
 
-        private void Awake() 
+        [SerializeField]
+        private float _ActiveTime;
+
+        [Space]
+        [SerializeField]
+        UnityEvent _OnHideEvent;
+
+        private Coroutine _Coroutine;
+
+        private void OnEase(float easeValue)
         {
-               
+            _Panel.color = _FadeGradient.Evaluate(easeValue);
         }
 
-        public void Hide()
+        private IEnumerator SplashScreenSequence()
         {
-            // _Director.Stop();
+            yield return new WaitForSeconds(2);
+
+            yield return VFXUtilities.LinearEaseIn(OnEase, () => Time.deltaTime, null, _FadeInTime);
+
+            yield return new WaitForSeconds(_ActiveTime);
+
+            Hide();
         }
 
-        public void Show()
+        public override void Hide()
         {
-            // _Director.Play();
+            base.Hide();
+
+            if(_Coroutine != null)
+                StopCoroutine(_Coroutine);
+                
+            _Coroutine = StartCoroutine(VFXUtilities.LinearEaseIn(OnEase, () => Time.deltaTime, null, _FadeOutTime, () => _OnHideEvent?.Invoke()));
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            
+            if(_Coroutine != null)
+                StopCoroutine(_Coroutine);
+
+            _Coroutine = StartCoroutine(SplashScreenSequence());
+        }
+
+        public void OnSubmit()
+        {
+            if(isShown)
+                Hide();
         }
     }
 }
