47d06b821b37ea0d42c9a6b1ab876534c5f92718
A-Cabigao <91714998+A-Cabigao@users.noreply.github.com>
2022-07-07 15:32:06 +0800

    Added radius for flight plane

--- Assets/CrossDestinyRevolution/Scripts/MovementSystem/FlightPlane/FlightPlane.cs
+++ Assets/CrossDestinyRevolution/Scripts/MovementSystem/FlightPlane/FlightPlane.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // This class handles FlightPlane methods and events for resize, move, and rotate.
+// NOTE: To ensure that radius works properly, make sure that x and z scale of flightPlane is similar
 
 namespace CDR.MovementSystem
 {
@@ -11,6 +12,8 @@ namespace CDR.MovementSystem
         private List<CharacterController>_activeControllers = new List<CharacterController>();
 
         public List<CharacterController> activeControllers => _activeControllers;
+
+        public float radius => CalculateRadius();
 
         Vector3 IFlightPlane.position => transform.position;
         Quaternion IFlightPlane.rotation => transform.rotation;
@@ -21,6 +24,18 @@ namespace CDR.MovementSystem
             {
                 _activeControllers.Add(controller);
             }
+        }
+
+        private float CalculateRadius()
+        {
+            return transform.localScale.x / 2f;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Debug.DrawLine(transform.position, 
+                new Vector3(transform.localScale.x, 0f, 0f) / 2f, 
+                Color.red, 99999f, false);
         }
     }
 }
