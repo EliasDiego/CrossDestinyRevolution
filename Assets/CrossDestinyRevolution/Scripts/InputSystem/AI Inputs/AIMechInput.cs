531cc4b91076989e4eba7e65964953e0f754e271
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-08-02 00:36:57 +0800

    Testing... ideas...

--- Assets/CrossDestinyRevolution/Scripts/InputSystem/AI Inputs/AIMechInput.cs
+++ Assets/CrossDestinyRevolution/Scripts/InputSystem/AI Inputs/AIMechInput.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -13,11 +14,43 @@ namespace CDR.InputSystem
     {   
         private IActiveCharacter _CurrentTarget;
 
+        private Vector3[] _DebugPositions;
         private Vector3 _DebugPosition;
         private Vector3 _DebugDirection;
 
+        private struct WeightedDirection
+        {
+            public Vector3 direction { get; }
+            public float weight { get; }
+            public float magnitude { get; }
+
+            public WeightedDirection(Vector3 direction, float magnitude, float weight)
+            {
+                this.direction = direction;
+                this.weight = weight;
+                this.magnitude = magnitude;
+            }
+
+            public WeightedDirection(Vector3 direction, float weight) : this(direction.normalized, direction.magnitude, weight) { }
+
+            public Quaternion GetRotation(Vector3 up)
+            {
+                return Quaternion.LookRotation(direction, up);
+            }
+
+            public Vector3 GetEulerAngles(Vector3 up)
+            {
+                return GetRotation(up).eulerAngles;
+            }
+        }
+
         private void OnDrawGizmos() 
         {
+            for(int i = 0; i < _DebugPositions.Length; i++)
+            {
+                Gizmos.DrawSphere(_DebugPositions[i], 5);
+            }
+
             if(_DebugDirection == Vector3.zero)
             {
                 Gizmos.color = Color.red;
@@ -32,11 +65,54 @@ namespace CDR.InputSystem
             }
         }
 
+        private Quaternion GetWeightedDirectionAverageRotation(params Vector3[] directions)
+        {
+            if(directions.Length <= 0)
+                return Quaternion.identity;
+
+            if(directions.Length == 1)
+            {
+                WeightedDirection weightedDirection = new WeightedDirection(directions[0], 1);
+
+                return weightedDirection.GetRotation(Vector3.up);
+            }
+
+            string debug = "";
+
+            float longestMagnitude = directions.Select(d => d.magnitude).Max();
+
+            Quaternion weightedRotation = Quaternion.identity;
+
+            foreach(WeightedDirection w in directions.Select(d => new WeightedDirection(d, 1 - (d.magnitude / longestMagnitude))).OrderByDescending(d => d.magnitude))
+            {
+                Quaternion rotation = w.GetRotation(Vector3.up);
+
+                weightedRotation = Quaternion.Slerp(weightedRotation, rotation, .5f);
+
+                debug += rotation.eulerAngles.y + "\n";
+            }
+
+            Debug.Log(weightedRotation.eulerAngles.y + "\n\n" + debug);
+
+            return weightedRotation; //Quaternion.Euler(weightedRotation.eulerAngles.x, weightedRotation.eulerAngles.y + 180, weightedRotation.eulerAngles.z);
+        }
+
         private float GetAsin(float value, float radius)
         {
             float sign = Mathf.Sign(value);
 
             return sign * Mathf.Min(Mathf.Asin(Mathf.Abs(value) / radius), radius);
+        }
+
+        private float GetPositiveDegree(float degree)
+        {
+            if(degree < 0)
+                degree += 360;
+
+            if(degree > 0)
+                return degree;
+
+            return GetPositiveDegree(degree);
         }
 
         private Vector3 GetBoundaryEdge()
@@ -61,23 +137,26 @@ namespace CDR.InputSystem
             if(_CurrentTarget == null)
                 return;
 
-            Vector3 dirAwayFromTarget = _CurrentTarget.position - character.position;
-            Vector3 dirAwayFromEdge = GetBoundaryEdge() - character.position;
+            Vector3 boundaryEdge = GetBoundaryEdge();
 
-            Quaternion rotAwayFromTarget = Quaternion.LookRotation(character.rotation * dirAwayFromTarget, Vector3.up);
-            Quaternion rotAwayFromEdge = Quaternion.LookRotation(character.rotation * dirAwayFromTarget, Vector3.up);
+            Vector3 dirAwayFromTarget = character.position - _CurrentTarget.position;
+            Vector3 dirAwayFromEdge = character.position - boundaryEdge;
 
-            Vector3 dirMove = Quaternion.Lerp(rotAwayFromEdge, rotAwayFromTarget, 0.5f) * -Vector3.forward;
-            // Vector3 dirMove = new Vector3(1, 0, 0);
-            
-            character.movement.Move(new Vector2(dirMove.x, dirMove.z));
+            Quaternion weightedRotation = GetWeightedDirectionAverageRotation(dirAwayFromTarget, dirAwayFromEdge);
 
+            Vector3 dirMove = weightedRotation * Vector3.forward;
+
+            character.controller.Translate(new Vector3(dirMove.x, 0, dirMove.z), .1f);
+
+            _DebugPositions[0] = boundaryEdge;
             _DebugPosition = character.position;
-            _DebugDirection = dirMove * 10;
+            _DebugDirection = dirMove * 30;
         }
 
         public override void SetupInput()
         {
+            _DebugPositions = new Vector3[5];
+
             AddAction("Move", OnMove);
 
             character.targetHandler.onSwitchTarget += OnSwitchTarget;
